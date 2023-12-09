f5c1d91505d437f04a30233a09ac5859024fca9d
NathanPrazeres <nathanprazeres@gmail.com>
2023-12-09 15:51:23 +0000

    Day 9

---
+++ 09/mirage_maintenance_1.py
@@ -0,0 +1,28 @@
+def parse(input_file:str="mirage_maintenance.in"):
+    with open(input_file, "r") as f:
+        lines = f.read().splitlines()
+    return [[int(x) for x in line.split()] for line in lines]
+
+def calculatePrediction(history:list):
+    diffs = [[] for _ in range(len(history))]
+    current_line = history
+    for i in range(len(current_line)):
+        for j in range(1, len(current_line)):
+            diffs[i].append(current_line[j] - current_line[j-1])
+        current_line = diffs[i]
+        if all([x == 0 for x in current_line]):
+            current_line.append(0)
+            break
+    diffs.reverse()
+    for i in range(0, len(diffs)-1):
+        if diffs[i] == []:
+            continue
+        diffs[i+1].append(diffs[i][-1] + diffs[i+1][-1])
+    return history[-1] + diffs[-1][-1]
+
+def solve(history_list:list):
+    return sum([calculatePrediction(history) for history in history_list])
+
+if __name__ == "__main__":
+    history_list = parse()
+    print(solve(history_list))
